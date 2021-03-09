fc349bc8269802f9c29ca26a0453ee6f79366c06
Roman Seo <hdsbsb@naver.com>
2021-03-09 18:06:38 +0900

    add reference

--- Javascript/Algorithm/Search.js
+++ Javascript/Algorithm/Search.js
@@ -1,9 +1,13 @@
-class Search {
-    constructor(input, size, func) {
-        this.input = input;
+// REFERENCE: https://www.zerocho.com/category/Algorithm/post/5870153c37e1c80018b64eb0
+// REFERENCE: https://ko.m.wikipedia.org/wiki/%EA%B9%8A%EC%9D%B4_%EC%9A%B0%EC%84%A0_%ED%83%90%EC%83%89
+
+export class GraphSearch {
+    constructor(graph, checkCondition, func) {
+        this.graph = graph;
         this.queue = new Array();
         this.stack = new Array();
         this.visited = new Array(size);
+        this.checkCondition = checkCondition;
         this.func = func;
     }
 
@@ -12,7 +16,7 @@ class Search {
         this.queue.push(current); 
         while (queue.length) {
             let current = this.queue.shift();
-            let result = this.func(current, this.input, this.visited);
+            let result = this.func(current, this.graph, this.visited);
             if(result) return result;
             this.bfs(current);
         }
@@ -22,11 +26,9 @@ class Search {
         this.visited[current] = true;
         this.stack.push(current); 
         let current = this.stack.pop();
-        let result = this.func(current, this.input, this.visited);
+        let result = this.func(current, this.graph, this.visited);
         if(result) return result;
         this.dfs(current);
     };
 
 }
-
-export default Search;
