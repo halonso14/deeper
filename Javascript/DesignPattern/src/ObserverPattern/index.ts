758caeba46cdd9fec0f4a7188086df68af2a20fe
Roman Seo <hdsbsb@naver.com>
2021-03-10 14:33:45 +0900

    fix: refactor ObserverPattern

--- Javascript/DesignPattern/src/ObserverPattern/index.ts
+++ Javascript/DesignPattern/src/ObserverPattern/index.ts
@@ -4,5 +4,22 @@ import DisplayElement from './interface/DisplayElement';
 import Observer from './interface/Observer';
 import Subject from './interface/Subject';
 
- export { CurrentConditionsDisplay, WeatherData, DisplayElement, Observer, Subject };
+function example(): void {
+    const weatherData: WeatherData = new WeatherData();
 
+    const display1 = new CurrentConditionsDisplay(weatherData);
+    const display2 = new CurrentConditionsDisplay(weatherData);
+    const display3 = new CurrentConditionsDisplay(weatherData);
+
+    console.log('display1 initialized.');
+    display1.display();
+    console.log('display2 initialized.');
+    display2.display();
+    console.log('display3 initialized.');
+    display3.display();
+
+    console.log('weather data updated.')
+    weatherData.setMeasurements(50, 10, 900);
+}
+
+export { CurrentConditionsDisplay, WeatherData, DisplayElement, Observer, Subject, example };
--- Javascript/DesignPattern/src/ObserverPattern/types.ts
+++ Javascript/DesignPattern/src/ObserverPattern/types.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_TEMPERATURE = 0;
+export const DEFAULT_TEMPERATURE = 32;
 export const DEFAULT_HUMIDITY = 50;
-export const DEFAULT_PRESSURE = 1000;
+export const DEFAULT_PRESSURE = 1023;
