6d766f3268aa9718dcb2d279076512f7d58ad272
Roman Seo <hdsbsb@naver.com>
2021-03-10 14:12:09 +0900

    feat: add example script

--- Javascript/DesignPattern/index.ts
+++ Javascript/DesignPattern/index.ts
@@ -1,4 +1,17 @@
-import { WeatherData } from './src/ObserverPattern';
+import { CurrentConditionsDisplay, WeatherData } from './src/ObserverPattern';
 
 const weatherData: WeatherData = new WeatherData();
-console.log(weatherData);
+
+const display1 = new CurrentConditionsDisplay(weatherData);
+const display2 = new CurrentConditionsDisplay(weatherData);
+const display3 = new CurrentConditionsDisplay(weatherData);
+
+console.log('display1 initialized.');
+display1.display();
+console.log('display2 initialized.');
+display2.display();
+console.log('display3 initialized.');
+display3.display();
+
+console.log('weather data updated.')
+weatherData.setMeasurements(50, 10, 900);
