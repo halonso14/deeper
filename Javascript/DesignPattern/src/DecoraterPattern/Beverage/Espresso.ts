c70e41d1bf5ad9315a270582fd6d31252ab23b46
halonso14 <hdsbsb@naver.com>
2021-03-11 22:52:48 +0900

    feat: prototype for Decorator Pattern

---
+++ Javascript/DesignPattern/src/DecoraterPattern/Beverage/Espresso.ts
@@ -0,0 +1,15 @@
+/* eslint-disable class-methods-use-this */
+import Beverage from './Beverage';
+
+class Espresso extends Beverage {
+  constructor() {
+    super();
+    this.description = 'Espresso';
+  }
+
+  cost(): number {
+    return 1.99;
+  }
+}
+
+export default Espresso;
