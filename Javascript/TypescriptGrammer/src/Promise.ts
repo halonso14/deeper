cba81596f79d1e41f1d308a3ab22f8f71cf046ba
halonso14 <hdsbsb@naver.com>
2021-03-30 00:12:33 +0900

    feat: Add Promise.all and Promise.finally

--- Javascript/TypescriptGrammer/src/Promise.ts
+++ Javascript/TypescriptGrammer/src/Promise.ts
@@ -1,5 +1,5 @@
 // try-catch is implicitly applied.
-const promiseErrorHandling = new Promise((resolve, reject) => {
+const promiseErrorHandling = new Promise((resolve: any, reject: any) => {
   console.log('Promise - Error handling.');
   let second = Math.random() * 4;
   // do something asynchronus.
@@ -19,43 +19,66 @@ const promiseErrorHandling = new Promise((resolve, reject) => {
       reject(new Error('This reject will be ignored.'));
       return;
     },
-    1000,
-    true
+    1000
   );
-})
   // onFirstRejectd will be skipped.
-  .then((onFirstFulfilled: any) => {
-    console.log('On first Fulfilled.');
-    console.log(onFirstFulfilled + 's');
-    if (onFirstFulfilled > 3) {
-      return onFirstFulfilled;
-    }
-    // throw goes to catch.
-    throw Error('Error from first Fulfilled.');
-  })
-  .then(
-    (onSecondFulfilled) => {
-      console.log('On second Fulfilled.');
-      console.log(onSecondFulfilled);
-    },
-    (onSecondRejected) => {
-      console.log('On second Rejected.');
-      console.log(`"${onSecondRejected}" is passed to second then`);
-      throw Error('Error from second Rejected.');
-    }
-  )
-  .then(
-    (onThirdFulfilled) => {
-      console.log('On third Fulfilled.');
-      console.log(onThirdFulfilled);
-    },
-    (onThirdRejected) => {
-      console.log('On third Rejected.');
-      console.log(onThirdRejected);
-      console.log(`"${onThirdRejected}" is passed to third then`);
-      throw Error('Error from third Rejected.');
-    }
-  )
-  .catch((error) => {
+}).
+then((onFirstFulfilled: any) => {
+  console.log('On first Fulfilled.');
+  console.log(onFirstFulfilled + 's');
+  if (onFirstFulfilled > 3) {
+    return onFirstFulfilled;
+  }
+  // throw goes to onSecondRejected.
+  throw Error('Error from first Fulfilled.');
+}).
+then(
+  (onSecondFulfilled: any) => {
+    console.log('On second Fulfilled.');
+    console.log(onSecondFulfilled);
+  },
+  (onSecondRejected: any) => {
+    console.log('On second Rejected.');
+    console.log(`"${onSecondRejected}" is passed to second then`);
+    throw Error('Error from second Rejected.');
+  }
+).
+then(
+  (onThirdFulfilled: any) => {
+    console.log('On third Fulfilled.');
+    console.log(onThirdFulfilled);
+  },
+  (onThirdRejected: any) => {
+    console.log('On third Rejected.');
+    console.log(onThirdRejected);
+    console.log(`"${onThirdRejected}" is passed to third then`);
+    throw Error('Error from third Rejected.');
+  }
+).
+catch((error: any) => {
     console.log('catch', error);
-  });
+}).
+finally(() => {
+    console.log('On Finally.');
+});
+
+const delayedProcess = (delay: number) => {
+    const delayedTime: number = Math.random() + delay;
+    return new Promise<number>((resolve) => {
+        setTimeout(() => {
+            console.log('delayed process:', delayedTime);
+        }, delayedTime);
+        resolve(delay);
+    }).then((onfulfilled: any) => {
+        console.log('onfulfilled:', onfulfilled);
+        return onfulfilled;
+    });
+}
+
+console.log('Promise All');
+Promise.all([delayedProcess(2), delayedProcess(1), delayedProcess(3)]).
+then((promiseAllResult: any) => {
+    console.log('Promase All Result:', promiseAllResult);
+})
+
+
