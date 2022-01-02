97734d95aed1f846e9b149ee41de71ffacc278a2
halonso <hdsbsb@naver.com>
2022-01-02 22:26:39 +0900

    feat: UPDATE ChocolateBoiler class

--- Javascript/DesignPattern/src/SingletonPattern/class/ChocolateBoiler.ts
+++ Javascript/DesignPattern/src/SingletonPattern/class/ChocolateBoiler.ts
@@ -1,26 +1,65 @@
+/* eslint-disable no-console */
 export default class ChocolateBoiler {
-    private static _instance: ChocolateBoiler;
+  private static _instance: ChocolateBoiler;
 
-    private constructor() {
-    }
+  private empty: boolean;
 
-    static getInstance() {
-        if (ChocolateBoiler._instance) {
-            console.log('Chocolate Instance exists.');
-            return ChocolateBoiler._instance;
-        }
-        console.log('Chocolate Instance does not exist.');
-        ChocolateBoiler._instance = new ChocolateBoiler();
-        return ChocolateBoiler._instance;
-    }
+  private boiled: boolean;
 
-    static getInstanceWithThis() {
-        if (this._instance) {
-            console.log('Chocolate Instance exists.');
-            return this._instance;
-        }
-        console.log('Chocolate Instance does not exist.');
-        this._instance = new this();
-        return this._instance;
+  private constructor() {
+    this.empty = true;
+    this.boiled = false;
+  }
+
+  static getInstance(): ChocolateBoiler {
+    if (ChocolateBoiler._instance) {
+      console.log('Chocolate Instance exists.');
+      return ChocolateBoiler._instance;
     }
+    console.log('Chocolate Instance does not exist.');
+    ChocolateBoiler._instance = new ChocolateBoiler();
+    return ChocolateBoiler._instance;
+  }
+
+  static getInstanceWithThis(): ChocolateBoiler {
+    if (this._instance) {
+      console.log('Chocolate Instance exists.');
+      return this._instance;
+    }
+    console.log('Chocolate Instance does not exist.');
+    this._instance = new this();
+    return this._instance;
+  }
+
+  public isBoiled(): boolean {
+    console.log('Is ChocolateBoiler boiled?', this.boiled);
+    return this.boiled;
+  }
+
+  public isEmpty(): boolean {
+    console.log('Is ChocolateBoiler empty?', this.empty);
+    return this.empty;
+  }
+
+  public fill(): void {
+    if (this.isEmpty()) {
+      console.log('ChocolateBoiler is filled with ingredients.');
+      this.empty = false;
+      this.boiled = false;
+    }
+  }
+
+  public drain(): void {
+    if (!this.isEmpty() && this.isBoiled()) {
+      console.log('Drain boiled ingredients.');
+      this.empty = true;
+    }
+  }
+
+  public boil(): void {
+    if (!this.isEmpty() && !this.isBoiled()) {
+      console.log('Boil ingredients.');
+      this.boiled = true;
+    }
+  }
 }
